8812c02b5db03be9dd6700f4e37c8fb76b538985
Marko Ćurlin <marko.curlin@live.com>
2019-01-20 17:14:44 +0100

    organized the code

--- solution.py
+++ solution.py
@@ -1,40 +1,42 @@
 from collections import defaultdict
 import matplotlib.pyplot as plt
 from utils import *
+import sys
 
 
-CHANNEL_STRING = "CHANNELNAME"
-UNIT_STRING = "UNIT"
-CRANK_ANGLE = "Crank Angle"
+CHANNEL_STRING = 'CHANNELNAME'
+UNIT_STRING = 'UNIT'
+CRANK_ANGLE = 'Crank Angle'
+
 
 while True:
-    # userDefinedSource = input("Where is your data stored >> ")
-    userDefinedSource = "Data"
+    userDefinedSource = input('Where is your data stored >> ')
+    # userDefinedSource = 'Data'
     inputFiles = getAllSourceFiles(userDefinedSource)
     if inputFiles is not None:
         break
-    print("The given folder does not exist!")
+    print('The given folder does not exist!')
 
-# userInputChannel = input("Which channel would you like to plot >> ")
-userInputChannel = ":flow:total_mass"  # delete this line
+userInputChannel = input('Which channel would you like to plot >> ')
+# userInputChannel = ':flow:total_mass'  # delete this line
 
 while True:
-    changeOutputLocation = input("Do you wish to change the output location (y/n) >> ")
+    changeOutputLocation = input('Do you wish to change the output location (y/n) >> ')
     if changeOutputLocation == 'y':
-        outputLocation = input("Please choose your new location >> ")
+        outputLocation = input('Please choose your new location >> ')
         if not Path(outputLocation).exists():
-            print("The selected output location does not exist!")
+            print('The selected output location does not exist!')
             continue
         break
     elif changeOutputLocation == 'n':
         outputLocation = os.path.dirname(__file__)
         break
 
-
+speedCases = None
 while True:
-    changeSpeedCases = input("Would you like to change which speed cases are processed (y/n) >> ")
+    changeSpeedCases = input('Would you like to change which speed cases are processed (y/n) >> ')
     if changeSpeedCases == 'y':
-        speedCases = input("Which speed cases would you like to use (ex. 3000 3500 3750) >> ")
+        speedCases = input('Which speed cases would you like to use (ex. 3000 3500 3750) >> ')
         speedCases = speedCases.split()
         break
     elif changeSpeedCases == 'n':
@@ -50,54 +52,62 @@ for filePath in inputFiles:
         tempParameters = {}
         tempData = {}
 
-        tempParameters["SPEED"] = os.path.basename(file.name).split("_")[4].rstrip("rpm")
+        tempParameters['SPEED'] = os.path.basename(file.name).split('_')[4].rstrip('rpm')
 
-        skip = False
+        skipFile = False
 
         for line in file:
-            if line.startswith("#"):
+            if line.startswith('#'):
                 continue
-            elif line.startswith("BEGIN"):
+            elif line.startswith('BEGIN'):
                 begin = True
-            elif line.startswith("END"):
-                if changeSpeedCases == "n":
+            elif line.startswith('END'):
+                if speedCases is None:
                     pass
-                elif tempParameters["SPEED"] not in speedCases:
-                    skip = True
+                elif tempParameters['SPEED'] not in speedCases:
+                    skipFile = True
                     break
                 begin = False
             elif begin:
-                splitLine = line.split(" = ")
+                splitLine = line.split(' = ')
                 if splitLine[0] == CHANNEL_STRING:
-                    allChannels = ""
-                    while line.endswith("&\n"):
-                        allChannels += line.rstrip("&\n")  # check if \n is needed. so far testing it worked without it
+                    allChannels = ''
+                    while line.endswith('&\n'):
+                        allChannels += line.rstrip('&\n')
                         line = next(file)
-                    allChannels += line.rstrip("\n")
-                    userChannelIndex = findChannel(userInputChannel, allChannels)
-                    crankAngleChannel = findChannel(CRANK_ANGLE, allChannels)
+                    allChannels += line.rstrip('\n')
+                    try:
+                        userChannelIndex = findChannel(userInputChannel, allChannels)
+                        crankAngleChannel = findChannel(CRANK_ANGLE, allChannels)
+                    except ValueError:
+                        skipFile = True
+                        break
                 elif splitLine[0] == UNIT_STRING:
-                    allUnits = ""
-                    while line.endswith("&\n"):
-                        allUnits += line.rstrip("&\n")
+                    allUnits = ''
+                    while line.endswith('&\n'):
+                        allUnits += line.rstrip('&\n')
                         line = next(file)
-                    allUnits += line.rstrip("\n")
+                    allUnits += line.rstrip('\n')
                     userUnit = findUnit(userChannelIndex, allUnits)
                     crankAngleUnit = findUnit(crankAngleChannel, allUnits)
-                    multiplier = 1000 if userUnit == "kg" else 1
+                    multiplier = 1000 if userUnit == 'kg' else 1
                 else:
-                    tempParameters[splitLine[0]] = ''.join(splitLine[1:]).rstrip("\n")
+                    tempParameters[splitLine[0]] = ''.join(splitLine[1:]).rstrip('\n')
             else:
                 splitLine = line.split()
                 tempData[float(splitLine[crankAngleChannel])] = float(splitLine[userChannelIndex]) * multiplier
 
-        if skip:
+        if skipFile:
             continue
         parameters.append(tempParameters)
         userChannelDataList.append(tempData)
 
-if userUnit == "kg":
-    userUnit = "g"
+if not userChannelDataList:
+    sys.exit("No data has been found! Finishing execution of this script!")
+
+
+if userUnit == 'kg':
+    userUnit = 'g'
 
 totalAverage = defaultdict(float)
 counter = defaultdict(int)
@@ -110,53 +120,15 @@ for dataDict in userChannelDataList:
 
 with cd(outputLocation):
 
-    with open("total_average_{0}.gid".format(userInputChannel.replace(":", '-')), "w") as outputFile:
-        outputLines = ["BEGIN\n", "CHANNELNAME = ['Crank Angle, '{0}']\n".format(userInputChannel),
-                       "UNIT = ['{0}', {1}]\n".format(crankAngleUnit, userUnit), "END\n"]
+    with open('total_average_{0}.gid'.format(userInputChannel.replace(':', '-')), 'w') as outputFile:
+        outputLines = ['BEGIN\n', 'CHANNELNAME = [\'Crank Angle, \'{0}\']\n'.format(userInputChannel),
+                       'UNIT = [\'{0}\', {1}]\n'.format(crankAngleUnit, userUnit), 'END\n']
         outputFile.writelines(outputLines)
         for key, count in counter.items():  # create an average for totalAverage
             totalAverage[key] /= count
-            outputFile.write("{0} {1}\n".format(key, totalAverage[key]))
-
-    # fig, ax = plt.subplots(len(userChannelDataList), 2)
-    # lengthOfUserChannelDataList = len(userChannelDataList)
-    # for i in range(lengthOfUserChannelDataList):
-    #     ax.item(i).plot(crankAngleDataList[i], userChannelDataList[i])
-    #
-    #     ax.item(i).set(xlabel='Crank Angle ({0})'.format(crankAngleUnit), ylabel='{0} ({1})'.format(userInputChannel, userUnit),
-    #                    title='Total Mass @ {0}'.format(parameters[i]["SPEED"]))
-    #     ax.item(i).grid()
-    #
-    # ax.item(lengthOfUserChannelDataList).plot(sorted(totalAverage.keys()), totalAverage.values())
-    # ax.item(lengthOfUserChannelDataList).set(xlabel='Crank Angle ({0})'.format(crankAngleUnit), ylabel='{0} ({1})'.format(userInputChannel, userUnit),
-    #                                          title='Average Total Mass')
-    # ax.item(lengthOfUserChannelDataList).grid()
-    #
-    # fig.delaxes(ax.item(3))
-    # fig.savefig("test.png")  # user defined location, else a default location
-    # plt.show()
-
-    # plotData = [*userChannelDataList, totalAverage]
+            outputFile.write('{0} {1}\n'.format(key, totalAverage[key]))
 
     lengthOfUserChannelDataList = len(userChannelDataList)
-
-    # fig, ax = plt.subplots(lengthOfUserChannelDataList + 1)
-    # for i in range(lengthOfUserChannelDataList):
-    #     ax.item(i).plot(sorted(userChannelDataList[i].keys()), userChannelDataList[i].values())  # order is preserved
-    #
-    #     ax.item(i).set(xlabel='Crank Angle ({0})'.format(crankAngleUnit),
-    #                    ylabel='{0} ({1})'.format(userInputChannel, userUnit),
-    #                    title='Total @ {0}'.format(parameters[i]["SPEED"]))
-    #     ax.item(i).grid()
-    #
-    # ax.item(lengthOfUserChannelDataList).plot(sorted(totalAverage.keys()), totalAverage.values())
-    # ax.item(lengthOfUserChannelDataList).set(xlabel='Crank Angle ({0})'.format(crankAngleUnit),
-    #                                          ylabel='{0} ({1})'.format(userInputChannel, userUnit),
-    #                                          title='Average Total')
-    # ax.item(lengthOfUserChannelDataList).grid()
-    #
-    # fig.savefig("graph.png")
-    # plt.show()
 
     for i in range(lengthOfUserChannelDataList):
         fig, ax = plt.subplots()
@@ -165,11 +137,10 @@ with cd(outputLocation):
 
         ax.set(xlabel='Crank Angle ({0})'.format(crankAngleUnit),
                ylabel='{0} ({1})'.format(userInputChannel, userUnit),
-               title='Total @ {0} rpm'.format(parameters[i]["SPEED"]))
+               title='Total @ {0} rpm'.format(parameters[i]['SPEED']))
         ax.grid()
         ax.legend()
-        fig.savefig("{0}rpm_graph.png".format(parameters[i]["SPEED"]))
-        # fig.canvas.set_window_title('Figure {0}'.format(i+1))
+        fig.savefig('{0}rpm_graph.png'.format(parameters[i]['SPEED']))
         plt.show()
 
     fig, ax = plt.subplots()
@@ -179,7 +150,6 @@ with cd(outputLocation):
            title='Average Total')
     ax.grid()
     ax.legend()
-
-    fig.savefig("Total_Average_graph.png")
-    # fig.canvas.set_window_title('Figure {0}'.format(lengthOfUserChannelDataList+1))
     plt.show()
+
+    fig.savefig('Total_Average_graph.png')
--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import re
 import os
 
 
-def getAllSourceFiles(source: str) -> list:
+def getAllSourceFiles(source: str) -> list or None:
     sourceDir = Path(source)
     if not sourceDir.exists():
         return None
@@ -23,7 +23,6 @@ def findChannel(searchChannel, allChannels):
             return counter
         counter += 1
     raise ValueError("The given channel [{0}] doesn't exist!".format(searchChannel))
-    # return None
 
 
 def findUnit(searchUnit, allUnits):
@@ -42,40 +41,3 @@ class cd:
 
     def __exit__(self, etype, value, traceback):
         os.chdir(self.savedPath)
-
-def readFile(file):
-    for line in file:
-        if line.startswith("#"):
-            continue
-        elif line.startswith("BEGIN"):
-            begin = True
-        elif line.startswith("END"):
-            if changeSpeedCases == "n":
-                pass
-            elif tempParameters["SPEED"] not in speedCases:
-                continue
-            begin = False
-        elif begin:
-            splitLine = line.split(" = ")
-            if splitLine[0] == CHANNEL_STRING:
-                allChannels = ""
-                while line.endswith("&\n"):
-                    allChannels += line.rstrip("&\n")  # check if \n is needed. so far testing it worked without it
-                    line = next(file)
-                allChannels += line.rstrip("\n")
-                userChannelIndex = findChannel(userInputChannel, allChannels)
-                crankAngleChannel = findChannel(CRANK_ANGLE, allChannels)
-            elif splitLine[0] == UNIT_STRING:
-                allUnits = ""
-                while line.endswith("&\n"):
-                    allUnits += line.rstrip("&\n")
-                    line = next(file)
-                allUnits += line.rstrip("\n")
-                userUnit = findUnit(userChannelIndex, allUnits)
-                crankAngleUnit = findUnit(crankAngleChannel, allUnits)
-                multiplier = 1000 if userUnit == "kg" else 1
-            else:
-                tempParameters[splitLine[0]] = ''.join(splitLine[1:]).rstrip("\n")
-        else:
-            splitLine = line.split()
-            tempData[float(splitLine[crankAngleChannel])] = float(splitLine[userChannelIndex]) * multiplier
